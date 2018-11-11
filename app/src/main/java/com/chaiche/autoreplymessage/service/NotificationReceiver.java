bd5eb5daf55d69f92d9bc698cae33b7846cb3467
chaiche <t89913085@gmail.com>
2018-11-11 12:27:48 +0800

    initial git

---
+++ app/src/main/java/com/chaiche/autoreplymessage/service/NotificationReceiver.java
@@ -0,0 +1,14 @@
+package com.chaiche.autoreplymessage.service;
+
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+
+public class NotificationReceiver extends NotificationListenerService {
+
+    @Override
+    public void onNotificationPosted(StatusBarNotification notification) {
+        super.onNotificationPosted(notification);
+
+
+    }
+}
